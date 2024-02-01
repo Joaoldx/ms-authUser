6c16282d67451271c88d659d2151eb49a90ee416
João Domingos <j13domingos@gmail.com>
2024-01-31 23:54:53 -0300

    fix: fixed variable name

--- src/main/java/com/ead/authUser/models/UserModel.java
+++ src/main/java/com/ead/authUser/models/UserModel.java
@@ -26,7 +26,7 @@ import lombok.Data;
 @Entity
 @Table(name = "TB_USERS")
 public class UserModel implements Serializable {
-    private static final long SerialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
