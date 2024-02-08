ff148727888bc7a48f01ade27554f601c78e7fd2
João Domingos <j13domingos@gmail.com>
2024-02-08 20:07:57 -0300

    feat: add pagiantion on endpoint FindAll

--- src/main/java/com/ead/authUser/services/UserService.java
+++ src/main/java/com/ead/authUser/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.ead.authUser.models.UserModel;
 
 public interface UserService {
 
     List<UserModel> findAll();
+    
+    Page<UserModel> findAll(Pageable pageable);
 
     Optional<UserModel> findById(UUID userId);
 
@@ -16,6 +21,5 @@ public interface UserService {
 
     boolean existsByEmail(String email);
 
-    boolean existsByUsername(String username);
-    
+    boolean existsByUsername(String username);    
 }
