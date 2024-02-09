e30377a5a9a92f3652efe1317b0952b1c4f78a93
João Domingos <j13domingos@gmail.com>
2024-02-09 16:49:21 -0300

    feat: extends specification executer

--- src/main/java/com/ead/authUser/repositories/UserRepository.java
+++ src/main/java/com/ead/authUser/repositories/UserRepository.java
@@ -3,10 +3,11 @@ package com.ead.authUser.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ead.authUser.models.UserModel;
 
-public interface UserRepository extends JpaRepository<UserModel, UUID> {
+public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
 
     boolean existsByEmail(String email);    
 
