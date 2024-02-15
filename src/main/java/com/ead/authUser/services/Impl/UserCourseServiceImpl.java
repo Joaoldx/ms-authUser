f163350d85653615cd5410acc135dfd614b553dd
João Domingos <j13domingos@gmail.com>
2024-02-14 22:09:12 -0300

    feat: add service and repository files

---
+++ src/main/java/com/ead/authUser/services/Impl/UserCourseServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ead.authUser.services.Impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ead.authUser.services.UserCourseService;
+
+public class UserCourseServiceImpl implements UserCourseService {
+    
+    @Autowired
+    UserCourseService userCourseService;
+}
