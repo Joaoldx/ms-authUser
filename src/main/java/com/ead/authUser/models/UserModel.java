6134b47dc2baf82e7570ef7f7d54fa8c4d65bc9f
João Domingos <j13domingos@gmail.com>
2024-02-25 20:24:43 -0300

    feat: create convert usercourse to usermodel

--- src/main/java/com/ead/authUser/models/UserModel.java
+++ src/main/java/com/ead/authUser/models/UserModel.java
@@ -78,4 +78,8 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<UserCourseModel> userCourses;
+
+    public UserCourseModel convertToUserCourseModel(UUID courseId) {
+        return new UserCourseModel(null, courseId, this);
+    }
 }
